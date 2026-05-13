43e4c2bb68835a6a1ad11161a0281f64abe75fae
runa.sasaki <runnpappa929@gmail.com>
2026-05-13 13:41:35 +0900

    fix: seed の $disconnect を await するように変更

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -16,4 +16,4 @@ async function main() {
 
 main()
     .catch((e) => { console.error(e); process.exit(1); })
-    .finally(() => prisma.$disconnect());
+    .finally(async () => { await prisma.$disconnect(); });
